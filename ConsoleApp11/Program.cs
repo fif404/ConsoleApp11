0877572cbad61045285afec5aec6d52b5153dc5a
fif <fif@DESKTOP-BFHUIKE>
2022-11-03 09:20:53 +1200

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,13 @@
+﻿// Скрипников Сергей 17вариант 2.1
+Console.WriteLine("Введите число A:");
+double A = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите B:");
+double B = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите D");
+double D = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите C");
+double C = double.Parse(Console.ReadLine());
+if (((A % 3 == 0) && (B % 5 == 0) && (D % 3 == 0) && (C % 5 == 0)) || ((A % 5 == 0) && (B % 3 == 0) && (D % 5 == 0) && (C % 3 == 0)))
+    Console.WriteLine("чилсо делится нацело");
+else
+    Console.WriteLine("число не делиться");
